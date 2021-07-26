1f47fd9a8564c91686e135c6c431d53f607ee7f7
HibiAdelei <lilyadelame@gmail.com>
2021-07-25 21:43:58 -0700

    updated Product.js Model

--- Develop/models/Product.js
+++ Develop/models/Product.js
@@ -10,7 +10,49 @@ class Product extends Model {}
 Product.init(
   {
     // define columns
+    id: {
+      type: DataTypes.INTEGER, 
+      allowNull: false, 
+      primaryKey: true,
+      autoIncrement: true
+    },
+
+      // defines product_name column
+    product_name: {
+      type: DataTypes.STRING,
+      allowNull: false
+    },
+      
+      // defines price column 
+    price: {
+      type: DataTypes.DECIMAL, 
+      allowNull: false,
+      validate: {
+       isDecimal: true
+      }  
+    },
+      // defintes stock column
+    stock: {
+      type: DataTypes.INTEGER,
+      allowNull: false, 
+      defaultValue: 10,
+      validate: {
+        isNumeric: true
+      }
+    },
+
+      // defines category id column
+    category_id: {
+      type: DataTypes.INTEGER,
+      // references Category model's id
+      references: {
+        model: 'category',
+        key: 'id'
+      }
+    },
+
   },
+
   {
     sequelize,
     timestamps: false,
